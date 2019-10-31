b9c67a380ee29a50f169ea52a739221a5901c07b
AmauryLC <amaury.le.chaffotec@laposte.net>
2019-10-31 17:11:28 +0100

    week end

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,32 @@
-
-
 import 'package:flutter/material.dart';
 
 import 'dart:async';
+
 
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite/sqflite.dart';
 
+import 'basededonne.dart';
 
- import 'basededonne.dart';
-
- 
-
-
-
-
-void main() async{
-//   Future<String> getDatabasesPath() => databaseFactory.getDatabasesPath();
-//   final Future<Database> database = openDatabase(
-//   // Set the path to the database. 
-//   join(await getDatabasesPath(), 'favorie.db'),
-//   // When the database is first created, create a table to store dogs.
-//   onCreate: (db, version) {
-//     // Run the CREATE TABLE statement on the database.
-//     return db.execute(
-//       "CREATE TABLE favorie(id INTEGER PRIMARY KEY, clee : TEXT)",
-//     );
-//   },
-//   // Set the version. This executes the onCreate function and provides a
-//   // path to perform database upgrades and downgrades.
-//   version: 1,
-// );
-
-  // var favorite  = [];
- var text = "";
-  void test(){
-
-  text = "bonjour";
-
-}
-
-
+void main() async {
   final dbHelper = Databasefavorie.instance;
- Future<List>  query() async {
-    var tab  = [];
+  Future<List> query() async {
+    var tab = [];
     final allRows = await dbHelper.queryAllRows();
-   
-     allRows.forEach((row) => tab.add(row['clee']) );
 
-    
+    allRows.forEach((row) => tab.add(row['clee']));
 
     return tab;
-      
-  
- 
   }
 
-  
+  Future<List> favorite1;
+  favorite1 = query().then((value) {
+    return value;
+  });
 
- 
+  var favorite = await favorite1;
 
-
-
-Future<List> favorite1 ;
-favorite1 =  query().then((value) { return value; }) ;
-
-var favorite = await favorite1;
-
-
-
-
-
-
-
-
-  
-
-  
-  
   runApp(MaterialApp(
     title: 'Flutter Tutorial',
     home: Home(favorite),
@@ -86,58 +34,34 @@ var favorite = await favorite1;
 }
 
 var tab1 = [
-  recette("test1","1"),
-  recette("test2","2"),
-  recette("test3","3"),
-  recette("test4","4"),
-  recette("test4","5"),
-  recette("test4","6"),
-
-
+  recette("Croque-monsieur", "1"," ", "assets/croc.jpg"),
+  recette("Tacos mexicains", "2"," ","assets/tacos.jpg"),
+  recette("Gaspacho au Thermomix", "3"," ","assets/gaspacho.jpg"),
+  recette("Tarte au thon", "4"," ","assets/tarte.jpg"),
+  recette("Samoussa rapide au boeuf", "5"," " , "assets/samoussa.jpg"),
+  // recette("test4", "6", null),
 ];
 
-
-
-
-
-
-
-
-
-
-
-
-
 class recette {
-
-  recette(this.titre,this.key);
-
+  recette(this.titre, this.key,this.soustitre ,this.photo);
 
   var titre;
+
+  var soustitre;
+
+  var photo;
+
   var key;
 }
 
-
-
 class Home extends StatelessWidget {
+  Home(this.favorite);
 
-Home(this.favorite);
-
-var  favorite;
-
-  
-
+  var favorite;
 
   @override
   Widget build(BuildContext context) {
-   
     // Scaffold is a layout for the major Material Components.
-
-
-
-
-
-    
 
     return Scaffold(
       appBar: AppBar(
@@ -149,7 +73,7 @@ var  favorite;
         padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
         children: tab1
             .map(
-              (x) => lignehomewidget(x,favorite),
+              (x) => lignehomewidget(x, favorite),
             )
             .toList(),
       ),
@@ -161,7 +85,9 @@ var  favorite;
             MaterialPageRoute(builder: (context) => Enterrecit(favorite)),
           );
         },
-        child: const Icon(Icons.add,  ),
+        child: const Icon(
+          Icons.add,
+        ),
         backgroundColor: Color.fromRGBO(240, 88, 93, 1),
       ),
 
@@ -184,9 +110,7 @@ var  favorite;
             ),
             ListTile(
               title: Text('Recettes'),
-              onTap: () {
-                
-              },
+              onTap: () {},
             ),
             ListTile(
               title: Text('Mes favories'),
@@ -203,12 +127,10 @@ var  favorite;
 }
 
 class Enterrecit extends StatelessWidget {
-
   Enterrecit(this.favorite);
 
   var favorite;
 
- 
   final myController = TextEditingController();
 
   var database;
@@ -246,8 +168,7 @@ class Enterrecit extends StatelessWidget {
               padding: const EdgeInsets.symmetric(vertical: 16.0),
               child: RaisedButton(
                 onPressed: () {
-                  
-                  tab1.add(recette(myController.text,"9"));
+                  tab1.add(recette(myController.text, "9",' ',' '));
                 },
                 child: Text('Submit'),
               ),
@@ -259,9 +180,19 @@ class Enterrecit extends StatelessWidget {
   }
 }
 
-class lignehomewidget extends StatefulWidget{
+class lignehomewidget extends StatefulWidget {
+  lignehomewidget(this.x, this.favorite);
 
-  lignehomewidget(this.x,this.favorite);
+  var x;
+
+  var favorite;
+
+  @override
+  Lignehome createState() => Lignehome(x, favorite);
+}
+
+class Lignehome extends State {
+  Lignehome(this.x, this.favorite);
 
   var x;
 
@@ -269,224 +200,129 @@ class lignehomewidget extends StatefulWidget{
 
   
 
-
-
-
-
-  @override
-  Lignehome createState() => Lignehome(x,favorite);
-  
-}
-
-class Lignehome extends State {
-
-  Lignehome(this.x,this.favorite);
-
-  
-
-  var x ;
-
-  var favorite;
-
-  
- 
-
- 
-
-  
-
-
-
-
-
-  void changement (){
-
+  void changement() {
     setState(() {
-
-         final dbHelper = Databasefavorie.instance;
+      final dbHelper = Databasefavorie.instance;
 
       void _insert(clee) async {
-    
-     await dbHelper.insert(clee);
-    
-  }
-
-  
-
-    void _delete(clee) async {
-    // Assuming that the number of rows is the id for the last row.
-
-   await dbHelper.delete(clee);
-  
-  }
-
-
-  // _insert();
-
-  // _delete();
-
-
-
-
-
-
-
-
-      if(!favorite.contains(x.key)){
-
-        favorite.add(x.key);
-         _insert(x.key);
-         print('insert');
-        
-
+        await dbHelper.insert(clee);
       }
-      else{
+
+      void _delete(clee) async {
+        // Assuming that the number of rows is the id for the last row.
+
+        await dbHelper.delete(clee);
+      }
+
+      // _insert();
+
+      // _delete();
+
+      if (!favorite.contains(x.key)) {
+        favorite.add(x.key);
+        _insert(x.key);
+        print('insert');
+      } else {
         favorite.remove(x.key);
         _delete(x.key);
         print('delete');
       }
-
-
-
-
-
-      
-      
-
     });
   }
 
-   @override
-  Widget build(BuildContext context) {
+  @override
+  Widget build(BuildContext context){
 
+
+    return Card( 
+
+      child: Column(
+        children: <Widget>[ListTile(
+
+          title: Text(x.titre),
+      subtitle: Text(x.titre) ,
+      trailing: IconButton(
+        icon: Icon(
+          favorite.contains(x.key) ? Icons.favorite : Icons.favorite_border,
+          color: favorite.contains(x.key) ? Colors.red : null,
+        ),
+        onPressed: () {
+          changement();
+        },
+      ),
+      onTap: () {
+         Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => Recettepresentation(x)));
+      },
+      
+      leading:  x.photo != null ? Image(image: AssetImage(x.photo),height: 100) : Text(""),
+          
+        
     
 
-    
-    
-    return  ListTile(
-                title: Text(x.titre),
-            
-    trailing: IconButton(
+        )],
+      )
 
-      icon:  Icon(
-              
-               favorite.contains(x.key) ? Icons.favorite : Icons.favorite_border,
-             color: favorite.contains(x.key)   ? Colors.red : null,
-              ) ,
-              onPressed: (){
+    );
 
-                
-                 changement();
-
-              },
-
-    ),
-                onTap: () {
-                  // Update the state of the app.
-                  // ...
-                  
-                },
-              );
 
   }
-  
+}
 
+// Container (
+//        child: x.photo != null ? Image(image: AssetImage(x.photo),height: 100,) : Text(""),
+        
+//       ),
+
+class Recettepresentation extends StatefulWidget {
+
+  Recettepresentation(this.recette);
+
+  var recette;
+
+  @override
+  Recettepresentationstate createState() => Recettepresentationstate(recette);
 
 }
 
+class Recettepresentationstate extends State {
+
+  Recettepresentationstate(this.recette);
+
+  var recette;
+
+ @override
+  Widget build(BuildContext context){
+
+     return Scaffold(
+      appBar: AppBar(
+        title: Text(recette.titre),
+        backgroundColor: Color.fromRGBO(240, 88, 93, 1),
+      ),
+      // body is the majority of the screen.
+      body:Column(
+
+        
+        
+        children: <Widget>[Container(
+          
+          child: recette.photo != null ? Image(image: AssetImage(recette.photo),width: 800,) : Text(""),
+          
+          ),Container( padding: EdgeInsets.symmetric(vertical: 35.0, horizontal: 8.0), child : Text('Ingrédients' , style: TextStyle(fontSize: 24),)),
+
+          
+          
+          
+          ],)
+
+      
+    );
+
+    
 
 
 
-// import 'package:flutter/material.dart';
-// // change `flutter_database` to whatever your project name is
-// import 'database_helper.dart';
+  }
 
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'SQFlite Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: MyHomePage(),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatelessWidget {
-
-//   // reference to our single class that manages the database
-//   final dbHelper = DatabaseHelper.instance;
-
-//   // homepage layout
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('sqflite'),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             RaisedButton(
-//               child: Text('insert', style: TextStyle(fontSize: 20),),
-//               onPressed: () {_insert();},
-//             ),
-//             RaisedButton(
-//               child: Text('query', style: TextStyle(fontSize: 20),),
-//               onPressed: () {_query();},
-//             ),
-//             RaisedButton(
-//               child: Text('update', style: TextStyle(fontSize: 20),),
-//               onPressed: () {_update();},
-//             ),
-//             RaisedButton(
-//               child: Text('delete', style: TextStyle(fontSize: 20),),
-//               onPressed: () {_delete();},
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-  
-//   // Button onPressed methods
-  
-//   void _insert() async {
-//     // row to insert
-//     Map<String, dynamic> row = {
-//       DatabaseHelper.columnName : 'Bob',
-//       DatabaseHelper.columnAge  : 23
-//     };
-//     final id = await dbHelper.insert(row);
-//     print('inserted row id: $id');
-//   }
-
-//   void _query() async {
-//     final allRows = await dbHelper.queryAllRows();
-//     print('query all rows:');
-//     allRows.forEach((row) => print(row));
-//   }
-
-//   void _update() async {
-//     // row to update
-//     Map<String, dynamic> row = {
-//       DatabaseHelper.columnId   : 1,
-//       DatabaseHelper.columnName : 'Mary',
-//       DatabaseHelper.columnAge  : 32
-//     };
-//     final rowsAffected = await dbHelper.update(row);
-//     print('updated $rowsAffected row(s)');
-//   }
-
-//   void _delete() async {
-//     // Assuming that the number of rows is the id for the last row.
-//     final id = await dbHelper.queryRowCount();
-//     final rowsDeleted = await dbHelper.delete(id);
-//     print('deleted $rowsDeleted row(s): row $id');
-//   }
-// }
+}
