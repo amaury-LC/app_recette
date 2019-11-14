eb076fc545a1e4a48df4c9161722745fdf86d2ce
AmauryLC <amaury.le.chaffotec@laposte.net>
2019-11-14 15:22:42 +0100

    ml suprim

--- lib/ml.dart
+++ lib/ml.dart
@@ -18,8 +18,8 @@ import 'entrer_recette.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import "mesfavoris.dart";
 import 'dart:convert';
-import 'package:image_picker/image_picker.dart';
-import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+// import 'package:image_picker/image_picker.dart';
+// import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 
 
 
@@ -38,40 +38,7 @@ class FacePage extends StatefulWidget{
 
 class _FacePageState extends State<FacePage> {
 
-    File _imageFile;
-
-    List<Face> _faces;
-
-    void _getImageAndDetectFaces() async {
-
-     final imageFile = await ImagePicker.pickImage(
-              source: ImageSource.gallery,
-                  
-            );
-      final image = FirebaseVisionImage.fromFile(imageFile);
-      final faceDetector = FirebaseVision.instance.faceDetector(
-
-              FaceDetectorOptions(
-
-                mode: FaceDetectorMode.accurate,
-              )
-            );
-            
-             final faces = await faceDetector.detectInImage(image);
-          if(mounted){
-            setState(() {
-              _imageFile = imageFile;
-              _faces = faces;
-            });
-
-          }
-     
-
-
-
-  }
-
-
+    
 
  
   @override
@@ -88,11 +55,13 @@ class _FacePageState extends State<FacePage> {
          backgroundColor: Color.fromRGBO(240, 88, 93, 1),
         ),
 
-        body: ImagesAndFaces(),
+        
 
         floatingActionButton: FloatingActionButton(
 
-          onPressed: _getImageAndDetectFaces,
+          onPressed: (){
+            
+          },
           tooltip: 'pick an image',
           child: Icon(Icons.add_a_photo),
           backgroundColor: Color.fromRGBO(240, 88, 93, 1),
@@ -110,67 +79,6 @@ class _FacePageState extends State<FacePage> {
 }
 
 
-class ImagesAndFaces extends StatelessWidget {
-
-  ImagesAndFaces({this.imageFile,this.faces});
-  
-  final  File imageFile;
-
-  final  List<Face> faces;
-
-  @override
-  Widget build(BuildContext){
-    return Column(children: <Widget>[
-
-      Flexible(
-
-        flex: 2,
-        child : Container(
-
-          constraints: BoxConstraints.expand(),
-
-          child:  Image.file(
-          imageFile,
-          fit: BoxFit.cover,
-        ),
-      ),
-        ),
-        
-      Flexible(
-
-        flex: 1,
-
-        child: ListView(
-
-          children: faces.map<Widget>((f) => FacesCoordinates(f)).toList(),
-        ),
-
-      )
-
-    ],);
-  }
-}
 
 
-class FacesCoordinates extends StatelessWidget {
 
-  FacesCoordinates(this.face);
-
-
-  final Face face;
-
-  @override
-
-  Widget build(BuildContext context){
-
-    final pos = face.boundingBox;
-    return ListTile(
-
-      title: Text('(${pos.top}, ${pos.left}), (${pos.bottom}, ${pos.right})') ,
-
-
-    );
-  }
-
-
-}
