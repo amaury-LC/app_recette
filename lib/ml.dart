ae4a03c76c90450b48ca2b19a6635d361ffec758
amaury-LC <amaury.le.chaffotec@laposte.net>
2019-11-18 17:57:11 +0100

    model perso

--- lib/ml.dart
+++ lib/ml.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
 
 import 'dart:async';
 
@@ -21,15 +23,14 @@ import 'dart:convert';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:image_picker/image_picker.dart';
 // import 'package:firebase_livestream_ml_vision/firebase_livestream_ml_vision.dart';
-
+import 'package:tflite/tflite.dart';
 
 class FacePage extends StatefulWidget {
   @override
-  createState() =>_FacePageState();
+  createState() => _FacePageState();
 }
 
 class _FacePageState extends State<FacePage> {
-
   // FirebaseVision _vision;
   var text1 = "test";
 
@@ -45,88 +46,50 @@ class _FacePageState extends State<FacePage> {
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () async {
-
-
             final File imageFile = await ImagePicker.pickImage(
               source: ImageSource.camera,
             );
 
-            // final FirebaseVisionImage visionImage =
-            //     FirebaseVisionImage.fromFile(imageFile);
+            Tflite.close();
+            try {
+              String res = await Tflite.loadModel(
+                model: "assets/quant/model_unquant.tflite",
+                labels: "assets/quant/labels.txt",
+              );
+              print(res);
+            } on PlatformException {
+              print('Failed to load model.');
+            }
 
-            
-           
-            
-            
-            
+            var recognitions = await Tflite.runModelOnImage(
+                path: imageFile.path, // required
+                imageMean: 0.0, // defaults to 117.0
+                imageStd: 255.0, // defaults to 1.0
+                numResults: 2, // defaults to 5
+                threshold: 0.2, // defaults to 0.1
+                asynch: true // defaults to true
+                );
 
-            
+            // var recognitions = await Tflite.detectObjectOnImage(
+            //     path: imageFile.path, // required
+            //     model: "SSDMobileNet",
+            //     imageMean: 127.5,
+            //     imageStd: 127.5,
+            //     threshold: 0.4, // defaults to 0.1
+            //     numResultsPerClass: 2, // defaults to 5
+            //     asynch: true // defaults to true
+            //     );
 
-            
+            print('camera');
+            print(recognitions);
 
-            // final ImageLabeler labeler = FirebaseVision.instance.imageLabeler();
+            // String test = recognitions[0].toString();
 
-            // final List<ImageLabel> labels =
-            //     await labeler.processImage(visionImage);
-
-            // for (ImageLabel label in labels) {
-            //   final String text = label.text;
-            //   print('test photo text');
-
-            //   setState(() {
-            //     text1 = text;
-            //   });
-
-              
-
-            //   print(text);
-            //   final String entityId = label.entityId;
-            //   print('test photo entite');
-            //   print(entityId);
-            //   final double confidence = label.confidence;
-            //   print('test photo confidence');
-            //   print(confidence);
-            // }
-
-            // labeler.close();
-
-            // final FirebaseVisionImage visionImage = FirebaseVisionImage.fromFile(imageFile);
-
-            // print('photo');
-
-            // print(visionImage);
+            setState(() {
+              text1 = recognitions[0]['label'];
+            });
 
             //test
-
-
-            
-
-      
-
-
-
-            
-
-
-             
-               
-
-
-            
-
-
-            
-
-
-
-
-
-
-
-
-
-
-
           },
           tooltip: 'pick an image',
           child: Icon(Icons.add_a_photo),
